9360567323866f5cbbf460926b83809fbcbe3ac3
Freddy Ayala <frayala@google.com>
2025-12-19 14:21:33 +0100

    feat: Adjust weapon damage values and update weapon descriptions.

--- src/archive/doom/DoomWeapons.js
+++ src/archive/doom/DoomWeapons.js
@@ -13,10 +13,10 @@ export class Weapon {
 }
 
 export const WEAPONS = [
-    new Weapon("BLASTER", 90, 15, 0x00ff00, 'hitscan', -1), // Ultra-Fast Semi-Auto (90ms)
-    new Weapon("SHOTGUN", 600, 3, 0xffaa00, 'spread', 12), // Faster Pump
+    new Weapon("BLASTER", 90, 4, 0x00ff00, 'hitscan', -1), // Weak "Pea Shooter" (3 hits to kill)
+    new Weapon("SHOTGUN", 600, 2.5, 0xffaa00, 'spread', 12), // Heavy Hitter (Max ~62 dmg)
     new Weapon("LAUNCHER", 750, 40, 0xff0000, 'projectile', 4),
     // NEW WEAPONS
-    new Weapon("PLASMA", 25, 8, 0x00ffff, 'projectile_fast', 100), // Rapid fire machine gun style, more ammo
-    new Weapon("BFG 9000", 1250, 200, 0x00ff00, 'bfg', 5) // Slower but more devastating
+    new Weapon("PLASMA", 25, 6, 0x00ffff, 'projectile_fast', 100), // Fast but moderate damage
+    new Weapon("BFG 9000", 1250, 200, 0x00ff00, 'bfg', 5) // Screen wipe
 ];
